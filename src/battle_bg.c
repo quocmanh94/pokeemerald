3567d4a4198d8453d556d22b5ea23d942e69e193
quocmanh94 <quocmanhdang94@gmail.com>
2023-07-12 03:49:09 +0700

    Revert "# Feature: Show Type Effectiveness In Battle"
    This reverts commit 9a6b5b15fe1c88bb67389c73fb8318da3c9c0477.

--- src/battle_bg.c
+++ src/battle_bg.c
@@ -387,33 +387,6 @@ static const struct WindowTemplate sStandardBattleWindowTemplates[] =
         .paletteNum = 5,
         .baseBlock = 0x0350,
     },
-    [B_WIN_TYPE_SUPER_EFF] = { // super effective
-        .bg = 0,
-        .tilemapLeft = 21,
-        .tilemapTop = 57,
-        .width = 8,
-        .height = 2,
-        .paletteNum = 5,
-        .baseBlock = 0x02a0,
-    },
-    [B_WIN_TYPE_NOT_VERY_EFF] = {
-        .bg = 0,
-        .tilemapLeft = 21,
-        .tilemapTop = 57,
-        .width = 8,
-        .height = 2,
-        .paletteNum = 5,
-        .baseBlock = 0x02a0,
-    },
-    [B_WIN_TYPE_NO_EFF] = {
-        .bg = 0,
-        .tilemapLeft = 21,
-        .tilemapTop = 57,
-        .width = 8,
-        .height = 2,
-        .paletteNum = 5,
-        .baseBlock = 0x02a0,
-    },
     DUMMY_WIN_TEMPLATE
 };
 
@@ -634,33 +607,6 @@ static const struct WindowTemplate sBattleArenaWindowTemplates[] =
         .height = 6,
         .paletteNum = 5,
         .baseBlock = 0x0350,
-    },
-    [B_WIN_TYPE_SUPER_EFF] = { // super effective
-        .bg = 0,
-        .tilemapLeft = 21,
-        .tilemapTop = 57,
-        .width = 8,
-        .height = 2,
-        .paletteNum = 5,
-        .baseBlock = 0x02a0,
-    },
-    [B_WIN_TYPE_NOT_VERY_EFF] = {
-        .bg = 0,
-        .tilemapLeft = 21,
-        .tilemapTop = 57,
-        .width = 8,
-        .height = 2,
-        .paletteNum = 5,
-        .baseBlock = 0x02a0,
-    },
-    [B_WIN_TYPE_NO_EFF] = {
-        .bg = 0,
-        .tilemapLeft = 21,
-        .tilemapTop = 57,
-        .width = 8,
-        .height = 2,
-        .paletteNum = 5,
-        .baseBlock = 0x02a0,
     },
     DUMMY_WIN_TEMPLATE
 };
