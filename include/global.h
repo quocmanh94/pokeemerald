049f711573542ab22cfaa27a366e4b534537d72e
quocmanh94 <quocmanhdang94@gmail.com>
2023-08-15 00:06:01 +0700

    [ADD] Difficulty Mode

--- include/global.h
+++ include/global.h
@@ -513,6 +513,7 @@ struct SaveBlock2
              u16 optionsSound:1; // OPTIONS_SOUND_[MONO/STEREO]
              u16 optionsBattleStyle:1; // OPTIONS_BATTLE_STYLE_[SHIFT/SET]
              u16 optionsBattleSceneOff:1; // whether battle animations are disabled
+             u16 optionsDifficulty:2; // DIFFICULTY_[EASY/NORMAL/MEDIUM]
              u16 regionMapZoom:1; // whether the map is zoomed in
              //u16 padding1:4;
              //u16 padding2;
