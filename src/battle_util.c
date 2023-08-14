c0e526b527f1124e0447b97a3930205e79d390c1
quocmanh94 <quocmanhdang94@gmail.com>
2023-08-15 02:17:28 +0700

    [ADD] PKM requires a Fateful Encounter to be Legal

--- src/battle_util.c
+++ src/battle_util.c
@@ -3890,6 +3890,11 @@ static bool32 IsBattlerModernFatefulEncounter(u8 battlerId)
     if (GetMonData(&gPlayerParty[gBattlerPartyIndexes[battlerId]], MON_DATA_SPECIES, NULL) != SPECIES_DEOXYS
         && GetMonData(&gPlayerParty[gBattlerPartyIndexes[battlerId]], MON_DATA_SPECIES, NULL) != SPECIES_MEW)
             return TRUE;
+    if (!GetMonData(&gPlayerParty[gBattlerPartyIndexes[battlerId]], MON_DATA_MODERN_FATEFUL_ENCOUNTER, NULL))
+    {
+        bool32 isEventLegal = TRUE;
+        SetMonData(&gPlayerParty[gBattlerPartyIndexes[battlerId]], MON_DATA_MODERN_FATEFUL_ENCOUNTER, &isEventLegal);
+    }
     return GetMonData(&gPlayerParty[gBattlerPartyIndexes[battlerId]], MON_DATA_MODERN_FATEFUL_ENCOUNTER, NULL);
 }
 
