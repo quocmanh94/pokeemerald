55c5de5349dd6332a78d692bc7d2c30caa55b9ae
quocmanh94 <quocmanhdang94@gmail.com>
2023-08-15 03:35:12 +0700

    [ADD] Evolution in Mid-battle

--- src/evolution_scene.c
+++ src/evolution_scene.c
@@ -33,6 +33,7 @@
 #include "constants/songs.h"
 #include "constants/rgb.h"
 #include "constants/items.h"
+#include "battle_anim.h"
 
 extern struct Evolution gEvolutionTable[][EVOS_PER_MON];
 
@@ -250,7 +251,8 @@ void EvolutionScene(struct Pokemon *mon, u16 postEvoSpecies, bool8 canStopEvo, u
     gReservedSpritePaletteCount = 4;
 
     sEvoStructPtr = AllocZeroed(sizeof(struct EvoInfo));
-    AllocateMonSpritesGfx();
+    if (!gMain.inBattle || gMonSpritesGfxPtr == NULL)
+        AllocateMonSpritesGfx();
 
     GetMonData(mon, MON_DATA_NICKNAME, name);
     StringCopy_Nickname(gStringVar1, name);
@@ -631,6 +633,8 @@ enum {
 
 // Task data from CycleEvolutionMonSprite
 #define tEvoStopped data[8]
+#define LEFT_PKMN gBattlerPartyIndexes[GetBattlerAtPosition(B_POSITION_PLAYER_LEFT)]
+#define RIGHT_PKMN gBattlerPartyIndexes[GetBattlerAtPosition(B_POSITION_PLAYER_RIGHT)]
 
 static void Task_EvolutionScene(u8 taskId)
 {
@@ -645,6 +649,13 @@ static void Task_EvolutionScene(u8 taskId)
     {
         gTasks[taskId].tState = EVOSTATE_CANCEL;
         gTasks[sEvoGraphicsTaskId].tEvoStopped = TRUE;
+        if (gMain.inBattle && gBattleOutcome == 0)
+        {
+            if (gTasks[taskId].tPartyId == LEFT_PKMN)
+                gPlayerDoesNotWantToEvolveLeft = TRUE;
+            else if (gTasks[taskId].tPartyId == RIGHT_PKMN)
+                gPlayerDoesNotWantToEvolveRight = TRUE;
+        }
         StopBgAnimation();
         return;
     }
@@ -770,6 +781,14 @@ static void Task_EvolutionScene(u8 taskId)
             GetSetPokedexFlag(SpeciesToNationalPokedexNum(gTasks[taskId].tPostEvoSpecies), FLAG_SET_SEEN);
             GetSetPokedexFlag(SpeciesToNationalPokedexNum(gTasks[taskId].tPostEvoSpecies), FLAG_SET_CAUGHT);
             IncrementGameStat(GAME_STAT_EVOLVED_POKEMON);
+            if (gMain.inBattle && gBattleOutcome == 0)
+            {
+                u8 monId = gTasks[taskId].tPartyId;
+                if (monId == LEFT_PKMN)
+                    CopyPlayerPartyMonToBattleData(0, monId, FALSE);
+                else if (monId == RIGHT_PKMN)
+                    CopyPlayerPartyMonToBattleData(2, monId, FALSE);
+            }
         }
         break;
     case EVOSTATE_TRY_LEARN_MOVE:
@@ -782,7 +801,10 @@ static void Task_EvolutionScene(u8 taskId)
                 if (!(gTasks[taskId].tBits & TASK_BIT_LEARN_MOVE))
                 {
                     StopMapMusic();
-                    Overworld_PlaySpecialMapMusic();
+                    if (gMain.inBattle && gBattleOutcome == 0)
+                        PlayBattleBGM();
+                    else
+                        Overworld_PlaySpecialMapMusic();
                 }
 
                 gTasks[taskId].tBits |= TASK_BIT_LEARN_MOVE;
@@ -796,7 +818,16 @@ static void Task_EvolutionScene(u8 taskId)
                 else if (var == MON_ALREADY_KNOWS_MOVE)
                     break;
                 else
+                {
+                    if (gMain.inBattle && gBattleOutcome == 0)
+                    {
+                        if (gTasks[taskId].tPartyId == LEFT_PKMN)
+                            GiveMoveToBattleMon(&gBattleMons[0], var);
+                        else if (gTasks[taskId].tPartyId == RIGHT_PKMN)
+                            GiveMoveToBattleMon(&gBattleMons[2], var);
+                    }
                     gTasks[taskId].tState = EVOSTATE_LEARNED_MOVE;
+                }
             }
             else // no move to learn, or evolution was canceled
             {
@@ -811,13 +842,17 @@ static void Task_EvolutionScene(u8 taskId)
             if (!(gTasks[taskId].tBits & TASK_BIT_LEARN_MOVE))
             {
                 StopMapMusic();
-                Overworld_PlaySpecialMapMusic();
+                if (gMain.inBattle && gBattleOutcome == 0)
+                    PlayBattleBGM();
+                else
+                    Overworld_PlaySpecialMapMusic();
             }
             if (!gTasks[taskId].tEvoWasStopped)
                 CreateShedinja(gTasks[taskId].tPreEvoSpecies, mon);
 
             DestroyTask(taskId);
-            FreeMonSpritesGfx();
+            if (!gMain.inBattle || gBattleOutcome != 0)
+                FreeMonSpritesGfx();
             FREE_AND_SET_NULL(sEvoStructPtr);
             FreeAllWindowBuffers();
             SetMainCallback2(gCB2_AfterEvolution);
@@ -996,7 +1031,19 @@ static void Task_EvolutionScene(u8 taskId)
                     {
                         // Forget move
                         PREPARE_MOVE_BUFFER(gBattleTextBuff2, move)
-
+                        if (gMain.inBattle && gBattleOutcome == 0)
+                        {
+                            if (gTasks[taskId].tPartyId == LEFT_PKMN)
+                            {
+                                RemoveBattleMonPPBonus(&gBattleMons[0], var);
+                                SetBattleMonMoveSlot(&gBattleMons[0], gMoveToLearn, var);
+                            }
+                            else if (gTasks[taskId].tPartyId == RIGHT_PKMN)
+                            {
+                                RemoveBattleMonPPBonus(&gBattleMons[2], var);
+                                SetBattleMonMoveSlot(&gBattleMons[2], gMoveToLearn, var);
+                            }
+                        }
                         RemoveMonPPBonus(mon, var);
                         SetMonMoveSlot(mon, gMoveToLearn, var);
                         gTasks[taskId].tLearnMoveState++;
