0ad3a5a8c05256f46c1025ded7adac85f90e11c6
quocmanh94 <quocmanhdang94@gmail.com>
2023-08-19 16:58:37 +0700

    [ADD] Add Description submenu in Battle

--- include/constants/battle.h
+++ include/constants/battle.h
@@ -371,6 +371,7 @@
 #define B_WIN_TYPE_SUPER_EFF     24
 #define B_WIN_TYPE_NOT_VERY_EFF  25
 #define B_WIN_TYPE_NO_EFF        26
+#define B_WIN_MOVE_DESCRIPTION   27
 
 // The following are duplicate id values for windows that Battle Arena uses differently.
 #define ARENA_WIN_PLAYER_NAME      15
