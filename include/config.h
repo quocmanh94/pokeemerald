5840b1a818219a95efb1d52e9910b3fdd3957ea6
quocmanh94 <quocmanhdang94@gmail.com>
2023-12-26 09:18:09 +0700

    [ADD] Pokedex plus in HGSS style

--- include/config.h
+++ include/config.h
@@ -46,6 +46,9 @@
 
 // Uncomment to fix some identified minor bugs
 #define BUGFIX
+#define HGSS_DEX TRUE
+#define HGSS_DARK_MODE TRUE
+#define HGSS_HIDE_UNSEEN_EVOLUTION_NAMES FALSE
 
 // Various undefined behavior bugs may or may not prevent compilation with
 // newer compilers. So always fix them when using a modern compiler.
